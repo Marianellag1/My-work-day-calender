4930eb0552147ddebb8d0eb832cb30939fba6ab1
Mgamboa <marianellag400@gmail.com>
2022-09-10 23:54:30 -0500

    modified files and folders

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,2 +1,40 @@
 var today = moment();
+var time = "h:mm"
 $("#current-day").text(today.format('dddd, MMMM Do'));
+
+var saveButton = document.querySelector("tbody");
+var notes = document.querySelectorAll("textarea").value;
+var userWroteSpan = document.querySelector("#user-notes")
+
+renderLastWritten();
+
+function hourDisplay(moment) {
+    var presentTime = document.querySelector(".present");
+    var futureTime = document.querySelector(".future");
+    var pastTime = document.querySelector(".past");
+
+    if (time === presentTime) {
+
+    }
+}
+
+function renderLastWritten() {
+    var timeSlot = localStorage.getItem("timeSlot");
+
+    userWroteSpan.textContent = timeSlot;
+    document.body.appendChild(userWroteSpan);
+
+}
+
+saveButton.addEventListener("click", function (event) {
+    // alert("it works!")
+
+    if (event.target.matches(".saveBtn")) {// code= office hours
+        console.log(event.target.dataset.time);
+        var timeSlot = event.target.dataset.time;
+        console.log(event.target.parentElement.children[1].children[0].value);
+        var dataInSlot = event.target.parentElement.children[1].children[0].value;
+        localStorage.setItem("timeSlot" + timeSlot, dataInSlot);
+        renderLastWritten();
+    };
+});
